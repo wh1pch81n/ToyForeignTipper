5b336158d40894828c159d3b94c021b8adbabb62
wh1pch81n <wh1pch81n@gmail.com>
2014-09-05 12:02:54 -0700

    Initial Commit

---
+++ ForeignCountryTip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ForeignCountryTip
+//
+//  Created by Derrick Ho on 9/5/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ForeignCountryTip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ForeignCountryTip
+//
+//  Created by Derrick Ho on 9/5/14.
+//  Copyright (c) 2014 dnthome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
