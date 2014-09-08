0f0933ac229bebbe3ea15f7509ee0d16b52cb8d8
wh1pch81n <wh1pch81n@gmail.com>
2014-09-08 10:53:55 -0700

    simplify tableviewcontroller class

--- ForeignCountryTip/ExchangeTableViewController.m
+++ ForeignCountryTip/ExchangeTableViewController.m
@@ -9,6 +9,10 @@
 #import "ExchangeTableViewController.h"
 #import "DHExchangeRates.h"
 
+NSString *const kCurrencyName = @"kCurrencyName";
+NSString *const kCurrencyCountry = @"kCurrencyCountry";
+NSString *const kCurrencyRate = @"kCurrencyRate";
+
 @interface ExchangeTableViewController ()
 
 @property (strong, nonatomic) NSArray *dataSource;
@@ -17,45 +21,26 @@
 
 @implementation ExchangeTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    self.dataSource = [DHExchangeRates sharedInstance].currencies.allKeys;
-    self.dataSource = [self.dataSource sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSArray *arr = [DHExchangeRates sharedInstance].currencies.allKeys;
+    arr = [arr sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    NSMutableArray *mut = [NSMutableArray new];
+    for (int i = 0; i < arr.count; ++i) {
+        NSString *name = arr[i];
+        NSString *country = [[DHExchangeRates sharedInstance] currencies][name][@"name"];
+        NSString *rate = [[DHExchangeRates sharedInstance] currencies][name][@"rate"];
+        [mut addObject:@{kCurrencyCountry: country,
+                        kCurrencyName: name,
+                         kCurrencyRate: rate}];
+    }
+    self.dataSource = mut;
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    // Return the number of sections.
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    // Return the number of rows in the section.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.dataSource.count;
 }
 
@@ -63,68 +48,23 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
-    // Configure the cell...
-    cell.textLabel.text = [NSString stringWithFormat:@"%@: %@", self.dataSource[indexPath.row], [[[[DHExchangeRates sharedInstance] currencies] objectForKey:self.dataSource[indexPath.row]] objectForKey:@"rate"]];
-    cell.detailTextLabel.text = [[[[DHExchangeRates sharedInstance] currencies] objectForKey:self.dataSource[indexPath.row]] objectForKey:@"name"];
+    NSDictionary *dict = self.dataSource[indexPath.row];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@: %@",
+                           dict[kCurrencyName],
+                           dict[kCurrencyRate]];
+    cell.detailTextLabel.text = dict[kCurrencyCountry];
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (self.completionBlock) {
-        NSString *key = self.dataSource[indexPath.row];
+        NSString *key = self.dataSource[indexPath.row][kCurrencyName];
         self.completionBlock(key);
     }
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 - (IBAction)tappedBack:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
